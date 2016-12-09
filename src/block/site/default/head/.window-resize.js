c60675cf4e91860f1c2b3640ae0cb5dd0723ae9e
caezar47 <iss47@ya.ru>
2016-12-09 12:34:36 +0300

    add lp-yarus

--- src/block/site/default/head/.window-resize.js
+++ src/block/site/default/head/.window-resize.js
@@ -4,14 +4,14 @@ var h_window = $(window).height(),
 	h_navbar = $('.navbar-site').outerHeight(true),
 	h_header = $('.header-site').outerHeight(true),
 	h_footer = $('.footer-site').outerHeight(true),
+	h_project_nav = $('._bread__btn-project').outerHeight(true),
 	h_content_index = h_window - h_navbar,
+	h_content_yarus = h_window - h_project_nav,
 	h_content = h_window - h_navbar - h_footer;
 
 if (device.mobile() || device.tablet()) {
 	$('.navbar').addClass('navbar-fixed-top');
-	$('.navbar-btn__cols').appendTo('.navbar-header__row');
-
-	 
+	$('.navbar-btn__cols').appendTo('.navbar-header__row');	 
 } else {
 }
 if (device.mobile()) {
@@ -21,6 +21,11 @@ if (device.mobile()) {
 	$('.content-site').css("min-height", h_content_index);
 	$('.index-page-content').css("height", h_content_index);
 	$('.full-window').css("min-height", h_content_index);
+	$('._yarus__block-site').css("height", h_window);
+	$('._yarus__block-footer').css("height", h_content_yarus);
+	$('._yarus__block-header').css("height", h_content_index);
 }
+
+
 $(".navbar-fixed-top").autoHidingNavbar();
 $(".scroller").mCustomScrollbar();
