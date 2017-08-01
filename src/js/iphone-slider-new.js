ddd738cfb21ca878a08e6eadbb9645edcd5bfff0
caezar47 <iss47@ya.ru>
2017-08-01 10:07:39 +0300

    fix

--- src/js/iphone-slider-new.js
+++ src/js/iphone-slider-new.js
@@ -19,7 +19,7 @@ $(function() {
 			});
 			
 			var button = $('<button/>', {
-				class : 'iphone-slider__item-btn',
+				class : 'iphone-slider__dot-btn',
 			})
 				.attr('data-item-index', _index)
 			;
@@ -29,7 +29,7 @@ $(function() {
 			
 		});
 		
-		dots.on('click', '.iphone-slider__dot .iphone-slider__item-btn', null, function(event){
+		dots.on('click', '.iphone-slider__dot .iphone-slider__dot-btn', null, function(event){
 			event.preventDefault();
 			
 			var button = $(this);
@@ -61,61 +61,8 @@ $(function() {
 			
 		});
 		
-		dots.find('.iphone-slider__dot').eq(0).find('.iphone-slider__item-btn').trigger('click');
+		dots.find('.iphone-slider__dot').eq(0).find('.iphone-slider__dot-btn').trigger('click');
 		
 	});
-	
-	/*
-	var iphone = $('.iphone-slider__container');
-	
-	$(document.body).on('azbn.setActive', '.iphone-slider__container .iphone-slider__item', {}, function(event){
-		event.preventDefault();
-		
-		var el = $(this);
-		el.addClass('is--active');
-		
-		var next = el.next('.iphone-slider__item');
-		if(next.size()) {			
-			$('.iphone-slider .iphone-slider__blurscreen .iphone-slider__blurscreen-inner')
-				.attr('style', next.attr('data-style'));
-			
-		} else {			
-			$('.iphone-slider .iphone-slider__blurscreen .iphone-slider__blurscreen-inner')
-				.attr('style', iphone.find('.iphone-slider__item').eq(0).attr('data-style'));
-			
-		}
-		
-	});
-	$(document.body).on('click', '.iphone-slider__btn', {}, function(event){
-			event.preventDefault();
-			
-			var screens = iphone.find('.iphone-slider__item');
-			var active = screens.filter('.is--active');
-			
-			if(active.size()) {
-				
-				var next = active.next('.iphone-slider__item');
-				if(next.size()) {
-					
-					screens.removeClass('is--active');
-					next.trigger('azbn.setActive');
-					
-				} else {
-					
-					screens.removeClass('is--active');
-					screens.eq(0).trigger('azbn.setActive');
-					
-				}
-				
-			} else {
-				
-				screens.eq(0).trigger('azbn.setActive');
-				
-			}
-			
-	});
-	$('.iphone-slider__btn').trigger('click');
-	
-	*/
 	
 });
