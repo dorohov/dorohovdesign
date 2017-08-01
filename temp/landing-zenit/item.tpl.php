66fd8d65be103d0f4d81a10d7dba5293de01056b
caezar47 <iss47@ya.ru>
2017-08-01 07:59:27 +0300

    add lp-zenit

---
+++ temp/landing-zenit/item.tpl.php
@@ -0,0 +1,273 @@
+<?
+	$GLOBALS['footer_type'] = 'slide';
+?>
+
+<div class="msk-page-content padtop">
+	<div class="msk76__block-content"> 
+		<div class="_mskbc__block-content">		
+			<div class="_mskbc__bg-figure yellow round-item1"></div>
+			<div class="_mskbc__bg-figure white10 round-item2"></div>
+			<div class="_mskbc__bg-figure blue rect-item1"></div>
+			<div class="_mskbc__bg-figure blue rect-item2"></div>
+			<div class="_mskbc__bg-img msk_hb__bg"></div>
+			<div class="msk__heading-block _mskbc__block">
+				<div class="container _mskbc__container">
+					<div class="row msk_hb__awords-row ">
+						<div class="cols msk_hb__awords-cols ">
+							<div class="msk_hb__awords-item">
+								<? if(get_field('cssdesignawards', $id)){?>
+								<a href="<?=get_field('cssdesignawards', $id);?>" rel="nofollow" target="_blank" class="msk_hb__awords-item">
+									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-260.5 381.9 118.2 94.6" style="enable-background:new -260.5 381.9 118.2 94.6;">
+										<g>
+											<g>
+												<path d="M-206.9,449.2v-0.1c-0.7-8.2-12-11.3-12-11.3s0.9,3.3,6,10.8c-11.6,1-15.7,2.8-15.7,2.8s11.4,7.8,19.7,2.3
+													c0.4-0.3,0.8-0.6,1.2-0.8c0.2-0.1,0.3-0.2,0.4-0.4C-205.3,450.8-205.7,449.8-206.9,449.2z"/>
+												<path d="M-241.7,438.6c0,0,4.4,0,15.3,3.6c0,0,9.8,3.9,0.7,5.6C-234.9,449.5-241.7,438.6-241.7,438.6z"/>
+												<path d="M-228.3,430.5c0,0-0.5,3.6,1.3,12.9c0,0,2.2,8.4,4.6,1.2C-219.9,437.3-228.3,430.5-228.3,430.5z"/>
+												<path d="M-249,423.2c0,0,4.2,1.2,13.6,7.8c0,0,8.2,6.5-1,5.5C-245.7,435.6-249,423.2-249,423.2z"/>
+												<path d="M-233.7,419.3c0,0-1.5,3.3-2.5,12.7c0,0-0.4,8.7,4.1,2.4C-227.7,428.2-233.7,419.3-233.7,419.3z"/>
+												<path d="M-248.5,407.2c0,0,2.8,2.4,7.9,10.7c0,0,4.1,7.9-2.8,4C-250.3,418-248.5,407.2-248.5,407.2z"/>
+												<path d="M-235.2,409.3c0,0-2.4,2-6.5,9.1c0,0-3.4,6.6,2.3,3.3C-233.6,418.3-235.2,409.3-235.2,409.3z"/>
+												<path d="M-245.6,394.4c0,0,2,2.4,4.8,10.1c0,0,2.1,7.2-2.9,2.9C-248.8,403-245.6,394.4-245.6,394.4z"/>
+												<path d="M-234.9,398.2c0,0-2.3,1.3-6.9,6.5c0,0-3.9,5,1.4,3.1C-235,406-234.9,398.2-234.9,398.2z"/>
+												<path d="M-232.2,381.9c0,0-1.6,9.4-6.7,14.6C-243.9,401.6-237.9,385.7-232.2,381.9z"/>
+											</g>
+											<g>
+												<path d="M-195.1,449.2v-0.1c0.7-8.2,12-11.3,12-11.3s-0.9,3.3-6,10.8c11.6,1,15.7,2.8,15.7,2.8s-11.4,7.8-19.7,2.3
+													c-0.4-0.3-0.8-0.6-1.2-0.8c-0.2-0.1-0.3-0.2-0.4-0.4C-196.7,450.8-196.3,449.8-195.1,449.2z"/>
+												<path d="M-160.3,438.6c0,0-4.4,0-15.3,3.6c0,0-9.8,3.9-0.7,5.6C-167.1,449.5-160.3,438.6-160.3,438.6z"/>
+												<path d="M-173.7,430.5c0,0,0.5,3.6-1.3,12.9c0,0-2.2,8.4-4.6,1.2C-182.1,437.3-173.7,430.5-173.7,430.5z"/>
+												<path d="M-153,423.2c0,0-4.2,1.2-13.6,7.8c0,0-8.2,6.5,1,5.5C-156.3,435.6-153,423.2-153,423.2z"/>
+												<path d="M-168.3,419.3c0,0,1.5,3.3,2.5,12.7c0,0,0.4,8.7-4.1,2.4C-174.3,428.2-168.3,419.3-168.3,419.3z"/>
+												<path d="M-153.5,407.2c0,0-2.8,2.4-7.9,10.7c0,0-4.1,7.9,2.8,4C-151.7,418-153.5,407.2-153.5,407.2z"/>
+												<path d="M-166.8,409.3c0,0,2.4,2,6.5,9.1c0,0,3.4,6.6-2.3,3.3C-168.4,418.3-166.8,409.3-166.8,409.3z"/>
+												<path d="M-156.4,394.4c0,0-2,2.4-4.8,10.1c0,0-2.1,7.2,2.9,2.9C-153.2,403-156.4,394.4-156.4,394.4z"/>
+												<path d="M-167.1,398.2c0,0,2.3,1.3,6.9,6.5c0,0,3.9,5-1.4,3.1C-167,406-167.1,398.2-167.1,398.2z"/>
+												<path d="M-169.8,381.9c0,0,1.6,9.4,6.7,14.6C-158.1,401.6-164.1,385.7-169.8,381.9z"/>
+											</g>
+											<polygon points="-191.9,458.4 -193.5,459.9 -200.3,451.6 -199.7,451.1 	"/>
+											<polygon points="-209.2,459.6 -210.7,458 -202.3,451.4 -201.7,452 	"/>
+										</g>
+										<text transform="matrix(1.0176 0 0 1 -219.4287 421.3176)" class="msk_hb__awords-num">17</text>
+										<text transform="matrix(0.927 0 0 1 -220.1835 431.2372)" class="msk_hb__awords-date">APR 2017</text>
+										<text transform="matrix(0.927 0 0 1 -260.5164 476.481)" class="msk_hb__awords-name">SPECIAL KUDOS</text>
+									</svg>
+								</a>
+								<?}?>
+							</div>
+						</div>
+					</div>
+					<h1 class="msk_hb__heading"><? the_title();?></h1>
+					<div class="row">
+						<div class="cols msk_hb__cols">
+							<div class="text-block _mskbc__text-block msk_hb__text-block ">
+								<? the_content(); ?>
+							</div>
+						</div>
+					</div>
+				</div> 
+			</div> 
+			<div class="_mskbc__bg-img msk_hb__bg-imac"></div>
+			<div class="_mskbc__bg-img msk_hb__bg-macbook"></div>	
+			<div class="_mskbc__bg-figure green rect-item3"></div>
+			<div class="_mskbc__bg-figure blue rect-item4"></div>
+			<div class="msk__step1-block _mskbc__block step">
+				<div class="container _mskbc__container">
+					<? if(get_field('step1__heading', $id)){?>
+						<h2 class="_mskbc__heading msk_s1b__heading" data-step="01"><?=get_field('step1__heading', $id);?></h2>
+					<?}?>
+					<div class=" msk_s1b__inner">
+						<?=get_field('step1__note', $id);?> 
+					</div>
+				</div> 
+			</div>
+			<div class="_mskbc__bg-figure green rect-item3"></div>
+			<div class="_mskbc__bg-figure blue rect-item4"></div>
+			<div class="msk__step2-block _mskbc__block step">
+			<div class="container _mskbc__container">
+				<? if(get_field('step2__heading', $id)){?>
+					<h2 class="_mskbc__heading msk_s2b__heading" data-step="02"><?=get_field('step2__heading', $id);?></h2>
+				<?}?>
+				<div class=" msk_s2b__inner">
+					<div class="row msk_s2b__row">
+						<div class="cols msk_s2b__cols note">
+							<div class="msk_s2b__note"><?=get_field('step2__note', $id);?></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt1" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt1"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt2" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt2"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt3" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt3"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt4" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt4"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt5" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt5"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt6" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt6"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt7" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt7"></use></svg></div>
+						</div>
+						<div class="cols msk_s2b__cols ">
+							<div class="msk_s2b__icon"><svg class="icon-svg icon-eqt8" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#eqt8"></use></svg></div>
+						</div>
+					</div>
+				</div>
+				</div> 
+			</div>
+			<div class="_mskbc__bg-figure green rect-item3"></div>
+			<div class="_mskbc__bg-figure blue rect-item4"></div>
+			<div class="msk__step3-block _mskbc__block step">
+				<div class="container _mskbc__container">				
+				<? if(get_field('step3__heading', $id)){?>
+					<h2 class="_mskbc__heading msk_s3b__heading" data-step="03"><?=get_field('step3__heading', $id);?></h2>
+				<?}?>
+				<div class=" msk_s3b__inner">
+					<div class="row msk_s3b__row">
+						<div class="cols msk_s3b__cols previews">
+							<img src="<?=$this->path('img');?>/lp-msk76/step3-item1.jpg" alt="">
+						</div>
+						<div class="cols msk_s3b__cols note">
+							<div class="text-block msk_s3b__text-block _mskbc__text-block">
+								<?=get_field('step3__note', $id);?>	
+							</div>
+							<div class="row msk_s3b__note-row">
+								<div class="cols msk_s3b__note-cols">
+									<img src="<?=$this->path('img');?>/lp-msk76/step3-item2.jpg" alt="">
+								</div>
+								<div class="cols msk_s3b__note-cols">
+									<img src="<?=$this->path('img');?>/lp-msk76/step3-item3.jpg" alt="">
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+				</div> 
+			</div>
+			<div class="_mskbc__bg-figure white10 round-item3"></div>
+			<div class="_mskbc__bg-figure blue rect-item6"></div>
+			<div class="msk__step4-block _mskbc__block step">
+				<div class="container _mskbc__container">
+					<? if(get_field('step4__heading', $id)){?>
+						<h2 class="_mskbc__heading msk_s4b__heading" data-step="04"><?=get_field('step4__heading', $id);?></h2>
+					<?}?>
+					<? if(get_field('step4__video', $id)){?>
+						<div class="msk_s4b__video">
+							<video class="msk_s4b__video-player lp-video" id="msk76-video" preload="none" controls poster="<?=$this->path('img');?>/lp-msk76/poster.jpg">
+								<source src="/wp-content/themes/azbnbasetheme/video/<?=get_field('step4__video', $id);?>" type="video/mp4">  
+							</video>
+						</div>
+					<?}?>
+					
+					<div class="_mskbc__bg-figure green rect-item5"></div>
+					
+				</div> 
+			</div>
+			<div class="msk__reserv-block _mskbc__block step">
+				<div class="container _mskbc__container">
+					<? if(get_field('reserv__heading', $id)){?>
+						<h2 class="_mskbc__heading-sm msk_rvb__heading"><?=get_field('reserv__heading', $id);?></h2>
+					<?}?>
+					<div class="msk_rvb__inner">
+						<?=get_field('reserv__note', $id);?>
+					</div>
+					
+				</div> 
+				<div class="msk_rvb__owl ">
+					<div><img src="<?=$this->path('img');?>/lp-msk76/reserv-item1.jpg" alt=""></div>
+					<div><img src="<?=$this->path('img');?>/lp-msk76/reserv-item3.jpg" alt=""></div>
+					<div><img src="<?=$this->path('img');?>/lp-msk76/reserv-item2.jpg" alt=""></div>
+				</div>
+			</div>
+			<div class="msk__building-block _mskbc__block step">
+				<div class="container _mskbc__container">
+					
+					<? if(get_field('building__heading', $id)){?>
+						<h2 class="_mskbc__heading-sm msk_bdb__heading"><?=get_field('building__heading', $id);?></h2>
+					<?}?>
+					<div class="msk_bdb__inner">
+						<div class="text-block msk_bdb__text-block _mskbc__text-block">
+							<?=get_field('building__note', $id);?>
+						</div>
+						<div class="msk_bdb__img1"><img src="<?=$this->path('img');?>/lp-msk76/building-item1.png" alt=""></div>
+						<div class="msk_bdb__img2"><img src="<?=$this->path('img');?>/lp-msk76/building-item2.png" alt=""></div>
+					</div>
+				</div> 
+				<div class="_mskbc__bg-figure round-item4"></div>
+			</div>
+			<div class="msk__backlight-block _mskbc__block step">
+				<div class="container _mskbc__container">		
+					<div class="text-block msk_blb__text-block _mskbc__text-block">
+						<?=get_field('backlight__note', $id);?>
+					</div>
+					<? if(get_field('backlight__video', $id)){?>
+						<div class="msk_blb__video">
+							<video class="msk_blb__video-player lp-video" id="msk76-video-backlight" loop preload="none" controls poster="<?=$this->path('img');?>/lp-msk76/poster-backlight.jpg">
+								<source src="/wp-content/themes/azbnbasetheme/video/<?=get_field('backlight__video', $id);?>" type="video/mp4">   
+							</video>
+						</div>
+					<?}?>						
+				</div> 
+			</div>
+			<div class="msk__step5-block _mskbc__block step">
+				<div class="_mskbc__bg-figure green-dark rect-item7"></div>
+				<div class="_mskbc__bg-figure round-item5"></div>
+				<div class="container _mskbc__container">
+					<? if(get_field('step5__heading', $id)){?>
+						<h2 class="_mskbc__heading msk_s5b__heading" data-step="05"><?=get_field('step5__heading', $id);?></h2>
+					<?}?>
+					<div class="iphone-slider">
+						<div class="iphone-slider__note">
+							<div class="text-block msk_s5b__text-block _mskbc__text-block">
+								<?=get_field('step5__note', $id);?> 
+							</div>
+						</div>
+						<div class="iphone-slider__bg">
+							<img src="<?=$this->path('img');?>/lp-msk76/bg-iphone-slider.png" alt="">
+							<div class="iphone-slider__container">
+								<div class="iphone-slider__item" style="background-image:url(<?=$this->path('img');?>/lp-msk76/iphone-slider-item1.jpg)"></div>
+								<div class="iphone-slider__item" style="background-image:url(<?=$this->path('img');?>/lp-msk76/iphone-slider-item2.jpg)"></div>
+								<div class="iphone-slider__item" style="background-image:url(<?=$this->path('img');?>/lp-msk76/iphone-slider-item3.jpg)"></div>
+								<div class="iphone-slider__item" style="background-image:url(<?=$this->path('img');?>/lp-msk76/iphone-slider-item4.jpg)"></div>
+							</div>
+						</div>
+						<div class="iphone-slider__blurscreen">
+							<div class="iphone-slider__blurscreen-inner"></div>
+						</div>
+						<button type="button" class="iphone-slider__btn">
+							<span class="iphone-slider__btn-line"></span>
+							<span class="iphone-slider__btn-arrow"><svg class="icon-svg icon-msk76-arrow-right" role="img"><use xlink:href="<?=$this->path('img');?>/svg/sprite.svg#msk76-arrow-right"></use></svg></span>
+						</button>
+					</div>		
+				</div> 
+			</div>
+			<div class="msk__step6-block _mskbc__block step">
+				<div class="_mskbc__bg-figure green rect-item8"></div>
+				<div class="_mskbc__bg-figure blue rect-item9"></div>
+				<div class="container _mskbc__container">
+					<? if(get_field('step6__heading', $id)){?>
+						<h2 class="_mskbc__heading msk_s1b__heading" data-step="06"><?=get_field('step6__heading', $id);?></h2>
+					<?}?>
+					<div class="msk_s6b__inner">
+						<?=get_field('step6__note', $id);?> 	
+					</div>	
+					<?if (get_field('work_link', $id)){?>
+					<div class="msk_s6b__link-block">
+						<h3 class="msk_s6b__link-heading">Спасибо за внимание</h3>
+						<div><a href="http://<?=get_field('work_link', $id);?>/" target="_blank" class="msk_s6b__link-item"><?=get_field('work_link', $id);?></a></div>
+					</div>
+					<?}?>	
+				</div> 
+			</div>
+
+		</div>
+	</div>
+</div>
+
