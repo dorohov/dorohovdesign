74920dda165bb68d56a66ae0cacf7f0212d10d0f
dorohov <pavel@dorohovdesign.com>
2018-03-23 10:33:33 +0300

    anton.03.18

---
+++ src/js/wrestbild.slider.js
@@ -0,0 +1,28 @@
+$(window).ready(function () {
+  
+/*  
+
+    $(window).on('scroll', function () {
+
+    var window_top = $(window).scrollTop();
+    var advantages_top = $(".down_wrestbild--block").offset().top;
+    console.log(window_top, advantages_top);
+
+       if(window_top > advantages_top) {
+
+        $(".element_hide").addClass( 'wrestbild-hide');
+
+        $(".down_wrestbild--block").addClass('down_background');
+
+      } else {
+
+
+
+
+    };
+
+
+  });
+
+*/
+});
